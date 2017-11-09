ff65f77b94417ee27850796f940a15ae198a7bb1
Payal Prasad <iit2015052@iiita.ac.in>
2017-11-09 23:14:00 +0530

    Add files via upload

--- rules.pl
+++ rules.pl
@@ -2,27 +2,27 @@ isgaming(X):-
     ram(X,R),R>=8,
     hd_size(X,H),H>=512,
     screen_size(X,S),S>15,
-    (processor_brand(X,intel),(processor_model(X,"i5");processor_model(X,"i3");processor_model(X,"i7")));processor_brand(X,amd),
+    (processor_brand(X,'intel');processor_brand(X,'amd');processor_brand(X,'nvidia')),(processor_model(X,'i5');processor_model(X,'i3');processor_model(X,'i7')),
     gc_size(X,G),G>=2,
-    os(X,"windows").
+    os(X,'windows').
 
 isdev(X):-
     ram(X,R),R>=8,
     hd_size(X,H),H>=512,
     screen_size(X,S),S>14,
-    clock_speed(X,C),C>=2.3, 
-    (processor_brand(X,intel),(processor_model(X,"i5");processor_model(X,"i3"))),
+    clock_speed(X,C),C>=2.3,
+    (processor_brand(X,'intel'),(processor_model(X,'i5');processor_model(X,'i3'))),
     gc_size(X,G),G>=1.
-    
+
 isclerk(X):-
     ram(X,R),R>=2,
     hd_size(X,H),H>=512,
     screen_size(X,S),S>12,
-    processor_brand(X,intel),
-    os(X,"windows").
+    processor_brand(X,'intel'),
+    os(X,'windows').
 
 ishome(X):-
     ram(X,R),R>=4,
     hd_size(X,H),H>=512,
     screen_size(X,S),S>13,
-    os(X,"windows");os(X,"mac").
+    os(X,'windows');os(X,'mac').
